67aae33f75d679b165676fe4b2b28728c55b51c4
jonghwee <pjh52682428@gmail.com>
2022-06-30 01:45:49 +0900

    0630_출력

---
+++ jonghwee/PrintCat.java
@@ -0,0 +1,10 @@
+package jonghwee;
+
+public class PrintCat{
+    public static void main(String[] args){
+        System.out.println("\\    /\\");
+        System.out.println(" )  ( ')");
+        System.out.println("(  /  )");
+        System.out.println(" \\(__)|");
+    }
+}
---
+++ jonghwee/PrintDog.java
@@ -0,0 +1,11 @@
+package jonghwee;
+
+public class PrintDog {
+    public static void main(String[] args){
+        System.out.println("|\\_/|");
+        System.out.println("|q p|   /}");
+        System.out.println("( ⨀ )\"\"\"\\");
+        System.out.println("|\"^\"`    |");
+        System.out.println("||_/=\\\\__|");
+    }
+}
