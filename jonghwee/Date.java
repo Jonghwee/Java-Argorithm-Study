8af650f349c1c595a584c7d8af16647e32718e22
jonghwee <pjh52682428@gmail.com>
2022-06-30 01:14:36 +0900

    0630_2557,10699,7287

---
+++ jonghwee/Date.java
@@ -0,0 +1,11 @@
+package jonghwee;
+
+import java.time.LocalDate;
+
+public class Date {
+    public static void main(String[] args){
+
+        LocalDate date = LocalDate.now();
+        System.out.println(date);
+    }
+}
---
+++ jonghwee/Register.java
@@ -0,0 +1,8 @@
+package jonghwee;
+
+public class Register {
+    public static void main(String[] args){
+        System.out.println(6);
+        System.out.println("pjh2428");
+    }
+}
