4394cbe104da1e5c8131cef0c9a4b6ff40de3163
jonghwee <pjh52682428@gmail.com>
2022-07-04 23:53:50 +0900

    0704_반복, 빠른 입출력

---
+++ jonghwee/test_0704/Boj15552.java
@@ -0,0 +1,30 @@
+package jonghwee.test_0704;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class Boj15552 {
+    public static void main(String[] args) {
+        
+        try{
+            BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+            BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+            int T = Integer.parseInt(br.readLine());
+
+            for(int i=1;i<=T;i++){
+                StringTokenizer st = new StringTokenizer(br.readLine());
+                int A = Integer.parseInt(st.nextToken());
+                int B = Integer.parseInt(st.nextToken());
+
+                bw.write((A+B) + "\n");
+            }
+            bw.flush();
+        }catch(IOException e){
+            e.printStackTrace();
+        }
+    }
+}
