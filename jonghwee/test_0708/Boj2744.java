e2839cae5d14480c3424657094ca198b9171bb01
jonghwee <pjh52682428@gmail.com>
2022-07-08 18:09:12 +0900

    0708_2744

--- jonghwee/test_0708/Boj2744.java
+++ jonghwee/test_0708/Boj2744.java
@@ -7,18 +7,19 @@ public class Boj2744 {
         Scanner sc = new Scanner(System.in);
 
         String str = sc.nextLine();
+        StringBuilder newStr = new StringBuilder(str);
         
         for(int i=0;i<str.length();i++){
             char c1 = str.charAt(i);
             if(Character.isUpperCase(c1)){
                 char c2 = Character.toLowerCase(c1);
-                str = str.replace(c1, c2);
+                newStr.setCharAt(i, c2);
             }else if(Character.isLowerCase(c1)){
                 char c2 = Character.toUpperCase(c1);
-                str = str.replace(c1, c2);
+                newStr.setCharAt(i, c2);
             }
         }
-        System.out.println(str);
+        System.out.println(newStr);
         sc.close();
     }
 }
