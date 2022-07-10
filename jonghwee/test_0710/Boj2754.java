efe54780455f16ba4bbc135fc792600ba4535e44
jonghwee <pjh52682428@gmail.com>
2022-07-11 02:40:33 +0900

    0711_2754,10809

--- jonghwee/test_0710/Boj2754.java
+++ jonghwee/test_0710/Boj2754.java
@@ -1,13 +1,31 @@
 package jonghwee.test_0710;
 
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.HashMap;
+import java.util.Map;
 
 public class Boj2754 {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String grade = br.readLine();
 
-        String grade = sc.nextLine();
+        Map<String, Double> score = new HashMap<String, Double>();
+        score.put("A+", 4.3);
+        score.put("A0", 4.0);
+        score.put("A-", 3.7);
+        score.put("B+", 3.3);
+        score.put("B0", 3.0);
+        score.put("B-", 2.7);
+        score.put("C+", 2.3);
+        score.put("C0", 2.0);
+        score.put("C-", 1.7);
+        score.put("D+", 1.3);
+        score.put("D0", 1.0);
+        score.put("D-", 0.7);
+        score.put("F", 0.0);
 
-        if (grade)
+        System.out.println(score.get(grade));
     }
 }
---
+++ jonghwee/text_0711/Boj10809.java
@@ -0,0 +1,13 @@
+package jonghwee.text_0711;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Boj10809 {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String str = br.readLine();
+
+    }
+}
