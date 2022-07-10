0a34dec747cefcb54c076c493257ed9dd10ef58f
jonghwee <pjh52682428@gmail.com>
2022-07-10 23:56:23 +0900

    0710_2754

---
+++ jonghwee/test_0710/Boj2754.java
@@ -0,0 +1,13 @@
+package jonghwee.test_0710;
+
+import java.util.Scanner;
+
+public class Boj2754 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        String grade = sc.nextLine();
+
+        if (grade)
+    }
+}
