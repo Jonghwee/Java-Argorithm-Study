64cf7e6274477d3edc681cffb2d0bd0462126a3e
jonghwee <pjh52682428@gmail.com>
2022-07-24 23:57:22 +0900

    0724_Section01_7

---
+++ jonghwee/test_0724/Section01_7.java
@@ -0,0 +1,33 @@
+package jonghwee.test_0724;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class Section01_7 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        String str = br.readLine();
+        StringBuilder sb = new StringBuilder(str.length());
+
+        for(int i=0;i<str.length();i++){
+            char c = Character.toUpperCase(str.charAt(i));
+            sb.append(c); 
+        }
+
+        StringBuilder reverse = sb.reverse();
+        //sb.reverse()는 StringBuilder 객체를 리턴하기 때문에 toString을 통해 String으로 바꿔줄 수 있다. (출력값은 같음)
+        
+        if(sb==reverse){
+            bw.write("YES");
+        }else{
+            bw.write("NO");
+        }
+        bw.flush();
+        bw.close();
+    }
+}
