be9c010d768a5c9ad714d73a59f3f27f768a91d8
jonghwee <pjh52682428@gmail.com>
2022-07-28 20:45:12 +0900

    0728_Boj10818 수정

--- jonghwee/test_0728/Boj10818.java
+++ jonghwee/test_0728/Boj10818.java
@@ -8,17 +8,18 @@ import java.io.OutputStreamWriter;
 
 public class Boj10818{
     public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System. in));
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
         int N = Integer.parseInt(br.readLine());
         int[] list = new int[N];
-        int max = list[0];
-        int min = list[0];
 
         for(int i=0;i<N;i++){
             list[i] = Integer.parseInt(br.readLine());    
         }
+
+        int max = list[0];
+        int min = list[0];
 
         for(int i=0;i<list.length;i++){
             if(max<list[i]){
@@ -28,15 +29,13 @@ public class Boj10818{
                 min = list[i];
             }
         }
-        
-        for(int a : list){
-           System.out.println(a);
-        }
 
-        System.out.println(min);
-        System.out.println(max);
-        bw.write(min);
-        bw.write(max);
+        // System.out.println(min);
+        // System.out.println(max);
+
+        bw.write(String.valueOf(min)+"\n"); //Integer.toString() 도 가능
+        bw.write(Integer.toString(max));
+        //bw.write는 int만 출력하는 것이 불가능.
         bw.flush();
         bw.close();
     }
